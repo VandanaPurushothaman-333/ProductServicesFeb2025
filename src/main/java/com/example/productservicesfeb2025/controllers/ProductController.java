0144cc0a63a8feab79b75cd30701ba818e16d1e7
Vandana Purushothaman <vandanapurushothaman333@gmail.com>
2025-02-24 22:43:06 +0530

    Basic structure

--- src/main/java/com/example/productservicesfeb2025/controllers/ProductController.java
+++ src/main/java/com/example/productservicesfeb2025/controllers/ProductController.java
@@ -1,16 +1,43 @@
 package com.example.productservicesfeb2025.controllers;
 
 import com.example.productservicesfeb2025.models.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    @GetMapping("/products/{Id}")
-    public Product getProductById(long id) {
-
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") long id) {
+    return null;
     }
 
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping()
+    public Product createProduct(@RequestBody Product product) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public Product updateProduct(@PathVariable ("id") Long productId,
+                                 @RequestBody Product product) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable ("id") Long productId,
+                                  @RequestBody Product product) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+
+    }
 }
--- src/main/java/com/example/productservicesfeb2025/controllers/SampleController.java
+++ src/main/java/com/example/productservicesfeb2025/controllers/SampleController.java
@@ -1,4 +1,28 @@
 package com.example.productservicesfeb2025.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/say")
 public class SampleController {
+
+    @GetMapping("/hello/{name}/{times}")
+    public String sayHello(@PathVariable("name") String name,
+                           @PathVariable("times") int times) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            sb.append(" Hello ").append(name);
+        }
+        return sb.toString();
+    }
+
+    @GetMapping("/bye")
+    public String sayBye() {
+
+        return "Bye Everyone!";
+    }
 }
+
